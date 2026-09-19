84d189470d1559ba0adbab948e6882ff455d6c42
aryan <aryan@networknuts.net>
2026-09-19 14:48:45 +0000

    modified loop file

--- python_basics/understanding_loop.py
+++ python_basics/understanding_loop.py
@@ -1,7 +1,10 @@
 user_list = ["abumere","pijush","himanshu"]
 
-for whatever_you_want in user_list:
-    print(whatever_you_want[0])
+#for whatever_you_want in user_list:
+#   print(whatever_you_want[0])
 
-# for whatever-variable-makes-sense in list:
-    # do something with that variable
+
+for user in user_list:
+    print(user)
+    print(type(user))
+    print(user[0])
