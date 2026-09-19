8d372f390e9aa7672b42939eca6538716abd21cf
aryan <aryan@networknuts.net>
2026-09-19 13:26:56 +0000

    added printing and and vars code

---
+++ python_basics/variable_print.py
@@ -0,0 +1,8 @@
+user_name = input("Enter your name: ")
+user_location = input("Enter your location: ")
+
+# DOT FORMAT METHOD - PYTHON2 AND PYTHON3
+print("Your name is {} and your location is {}.".format(user_name,user_location))
+
+# F METHOD - PYTHON3
+print(f"Your name is {user_name} and your location is {user_location}.")
---
+++ python_basics/variable_test.py
@@ -0,0 +1,5 @@
+user_name = input("What is your name? ")
+user_id = 1001
+
+print(user_name)
+print(user_id)
